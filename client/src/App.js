e047bb89c7b09966738152b20d94ef33b45269e2
Jordan Thijssen <Jordan.Thijssen@esgglobal.com>
2024-02-02 00:38:45 +0000

    added form and responsive styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import './form.css';
 
 const App = () => {
+
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>FirstImpressions - AI powered digital marketing for your business</h1>
 
       </header>
-      <main>
-        <section>
+      <main className='container m-0'>
+        <section className='row'>
           <p>Our pipelines provide you with fast and frequent coverage on your socials. </p>
           <h2>Let's find a plan for you</h2>
           <div className="pricing-grid">
@@ -41,6 +49,38 @@ const App = () => {
               </ul>
             </div>
           </div>
+        </section>
+        <section className='form row'>
+        <form>
+    <span>Enquire below to receive your free AI Marketing InfoDoc</span>
+
+    <div class="form-group">
+      <label htmlFor="name">Name</label>
+      <input type="text"  name="name" id="name" required onChange={handleChange}/>
+    </div>
+
+    <div class="form-group">
+      <label htmlFor="email">Email</label>
+      <input type="email"  name="email" id="email" required onChange={handleChange}/>
+    </div>
+
+    <div class="form-group">
+      <label htmlFor="phone">Phone</label>
+      <input type="tel"  name="phone" id="phone" required onChange={handleChange}/>
+    </div>
+
+    <div class="form-group">
+      <label htmlFor="company">Company</label>
+      <input type="text"  name="company" id="company" required onChange={handleChange}/>
+    </div>
+
+    <div class="form-group">
+      <label htmlFor="message">Comments / Questions</label>
+      <textarea  name="message" id="message" required onChange={handleChange}></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
         </section>
       </main>
     </div>
