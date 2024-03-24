2bfd707a0c8a1bc6733a4b266a31868f1ac8161d
Jordan Thijssen <Jordan.Thijssen@esgglobal.com>
2024-03-24 12:05:11 +0000

    adding call webhook

--- api/missedCall.js
+++ api/missedCall.js
@@ -5,10 +5,12 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 router.post('/voice', (req, res) => {
     const twiml = new VoiceResponse();
     const city = req.body.FromCity;
+    const fromNo = req.body.From;
 
-    twiml.say(`Sorry you missed us, I'm probably busy right now ${city} but will get back to you as soon as I can \n - FirstImpression`)
-    console.log(`Missed call`);
-    
+    twiml.say(`Sorry you missed us, I'm probably busy right now, you live in ${city} and Number is ${fromNo} but will get back to you as soon as I can \n - FirstImpression`)
+    console.log(`Missed call from ${fromNo} in ${city}`);
+    res.type('text/xml');
+    res.send(twiml.toString());
     res.status(200).end();
 });
 
